33ae3e26be1c446a4d44ca91b1e14f54f592adb5
eugen-bondarev <eug.bondarev@gmail.com>
2022-02-15 13:03:46 +0100

    Some more examples, using sass and vue.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,49 @@
 $(document).ready(function() {
-    $('.preview').slick({
+    $(".preview").slick({
         dots: true,
         arrows: false
     });
 });
 
+const app = new Vue({
+    el: "#app",
+    data: () => {
+        return {
+            projects: [
+                {
+                    title: "Besondere Lernleistung im Fach Informatik (Abitur 2022)",
+                    links: [
+                        {
+                            cls: "fa-solid fa-file-lines",
+                            href: "materials/BLL_Eugen_Bondarev.pdf"
+                        },
+                        {
+                            cls: "fa-brands fa-github",
+                            href: "https://github.com/eugen-bondarev/BLL"
+                        }
+                    ],
+                    gallery: [
+                        "images/projects/bll_1.png",
+                        "images/projects/bll_0.png",
+                        "images/projects/bll_2.png",
+                    ],
+                    techStack: ["C++", "vcpkg", "CMake", "eigen", "OpenGL", "ImGui"]
+                },
+                {
+                    title: "Eine App fürs Teilen von Dateien innerhalb eines WLAN-Netzes",
+                    links: [
+                        {
+                            cls: "fa-brands fa-github",
+                            href: "https://github.com/eugen-bondarev/homeNetShare"
+                        }
+                    ],
+                    gallery: [
+                        "images/projects/homeNetShare_0.png",
+                        "images/projects/homeNetShare_1.png"
+                    ],
+                    techStack: ["Java", "maven", "OpenGL", "ImGui"]
+                }
+            ]
+        };
+    }
+});
